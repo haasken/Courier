4ddc0667ad5733258d12de1d209aa402b6606213
haasken <ryanhaasken@gmail.com>
2014-08-25 12:59:18 -0500

    Remove unused controller code

--- Controller.js
+++ Controller.js
@@ -20,11 +20,6 @@ var Controller = function() {
 	this.directionKeys[0] = (wasdDirs);
 	this.directionKeys[1] = (arrowDirs);
 
-	this.upKey = keyboard.KEY.W;
-	this.downKey = keyboard.KEY.S;
-	this.leftKey = keyboard.KEY.A;
-	this.rightKey = keyboard.KEY.D;
-
 	this.logging = false;
 	this.direction = DIRECTIONS.none;
 }
@@ -69,34 +64,5 @@ Controller.prototype.isTurning = function(direction) {
 	}
 	if (retVal && this.logging && console)
 		console.log("Controller::isTurning() returns true for direction ", direction)
-	return retVal;
-}
-
-Controller.prototype.isTurningUp = function() {
-	var retVal = keyboard.keyHeld(this.upKey) && ! keyboard.keyHeld(this.downKey);
-	if (retVal && this.logging && console)
-		console.log("Controller::isTurningUp() returns true.")
-	return retVal;
-}
-
-/* Turn down for what?? */
-Controller.prototype.isTurningDown = function() {
-	var retVal = keyboard.keyHeld(this.downKey) && ! keyboard.keyHeld(this.upKey);
-	if (retVal && this.logging && console)
-		console.log("Controller::isTurningDown() returns true.")
-	return retVal;
-}
-
-Controller.prototype.isTurningLeft = function() {
-	var retVal = keyboard.keyHeld(this.leftKey) && ! keyboard.keyHeld(this.rightKey);
-	if (retVal && this.logging && console)
-		console.log("Controller::isTurningLeft() returns true.")
-	return retVal;
-}
-
-Controller.prototype.isTurningRight = function() {
-	var retVal = keyboard.keyHeld(this.rightKey) && ! keyboard.keyHeld(this.leftKey);
-	if (retVal && this.logging && console)
-		console.log("Controller::isTurningRight() returns true.")
 	return retVal;
 }
